32ffc5a8c2f58350608dd37e3c540c4315038a4b
Mohammad Al Samiul <alsamiul123@gmail.com>
2024-09-06 12:12:50 +0600

    booking modal component added

--- src/pages/bikes/BikeDesc.tsx
+++ src/pages/bikes/BikeDesc.tsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useGetSingleBikeQuery } from "../../redux/features/bike/bikeApi";
 import { TBikeProps } from "./Bikes";
+import Modals from "../modals/Modals";
 
 const BikeDesc = () => {
   const { id } = useParams();
@@ -11,27 +12,38 @@ const BikeDesc = () => {
   return (
     <div className="hero flex justify-start  w-full shadow-xl rounded-lg ">
       <div className="hero-content flex-col lg:flex-row ">
-        <img src={bike.bikeImage} className="w-96 rounded-lg shadow-2xl" />
+        <img src={bike?.bikeImage} className="w-96 rounded-lg shadow-2xl" />
         <div>
-          <h1 className="text-5xl font-bold">{bike.name}</h1>
-          <p className="py-2">{bike.description}</p>
+          <h1 className="text-5xl font-bold">{bike?.name}</h1>
+          <p className="py-2">{bike?.description}</p>
           <p className="py-2">
-            <span className="font-bold">Brand :</span> {bike.description}
+            <span className="font-bold">Brand :</span> {bike?.brand}
           </p>
           <p className="py-2">
-            <span className="font-bold">Model :</span> {bike.model}
+            <span className="font-bold">Model :</span> {bike?.model}
           </p>
           <p className="py-2">
-            <span className="font-bold">CC :</span> {bike.cc}
+            <span className="font-bold">CC :</span> {bike?.cc}
           </p>
           <p className="py-2">
-            <span className="font-bold">Year :</span> {bike.year}
+            <span className="font-bold">Year :</span> {bike?.year}
           </p>
           <p className="py-2">
             <span className="font-bold">Price Per Hour :</span>$
-            {bike.pricePerHour}
+            {bike?.pricePerHour}
           </p>
-          <button className="btn btn-accent text-white">Booked Now</button>
+
+          <button
+            className="btn btn-accent text-white"
+            onClick={() =>
+              (
+                document.getElementById(bike?._id) as HTMLDialogElement
+              )?.showModal()
+            }
+          >
+            Booked Now
+          </button>
+          <Modals id={bike?._id} />
         </div>
       </div>
     </div>
